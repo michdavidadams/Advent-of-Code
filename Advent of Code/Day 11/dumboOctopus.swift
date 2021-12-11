8a979c851ecce6b1a20193676439fdbbf2477a14
Michael Adams <me@michdavidadams.com>
2021-12-11 16:34:30 -0500

    Started Day 11

---
+++ Advent of Code/Day 11/dumboOctopus.swift
@@ -0,0 +1,17 @@
+//
+//  dumboOctopus.swift
+//  Advent of Code
+//
+//  Created by Michael Adams on 12/11/21.
+//
+
+import Foundation
+
+func oneHundredSteps(flashes: [[[Int]]]) {
+   var step = 0
+    for i in 0...100 {
+        for row in flashes {
+            if 
+        }
+    }
+}
