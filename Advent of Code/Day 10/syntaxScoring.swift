9e783927ddecedf0ee6fb6a53585cafa5dc958b3
Michael Adams <me@michdavidadams.com>
2021-12-12 19:23:40 -0500

    Day 10 Part 2

--- Advent of Code/Day 10/syntaxScoring.swift
+++ Advent of Code/Day 10/syntaxScoring.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
+// For finding middle of array
+extension Array {
+    var middle: Element? {
+        guard count != 0 else { return nil }
+        let middleIndex = (count > 1 ? count - 1 : count) / 2
+        return self[middleIndex]
+    }
+}
+
 // MARK: - Part 1
 func firstIllegalCharacter(navigationSubsystem: [String]) {
     var lineComplete = false
@@ -57,4 +66,67 @@ func firstIllegalCharacter(navigationSubsystem: [String]) {
     }
     
     print(points)
+}
+
+// MARK: - Part 2
+func completeIncorrectLines(navigationSubsystem: [String]) {
+    var lineComplete = false
+    var expectedCharacters = ""
+    var linePoints = 0
+    var points: [Int] = []
+    print(navigationSubsystem.count)
+    
+    for line in navigationSubsystem {
+        expectedCharacters = ""
+        lineComplete = false
+        linePoints = 0
+        for char in line.indices {
+            if lineComplete == false {
+                if line[char] == "<" || line[char] == "[" || line[char] == "(" || line[char] == "{" {
+                    switch line[char] {
+                    case "<":
+                        expectedCharacters.append(">")
+                    case "[":
+                        expectedCharacters.append("]")
+                    case "(":
+                        expectedCharacters.append(")")
+                    case "{":
+                        expectedCharacters.append("}")
+                    default:
+                        return
+                    }
+                } else if line[char] == ">" || line[char] == "]" || line[char] == ")" || line[char] == "}" {
+                    if line[char] == expectedCharacters.last {
+                        expectedCharacters.removeLast()
+                    } else {
+                        lineComplete = true
+                    }
+                }
+            }
+        }
+        let reversed = String(expectedCharacters.reversed())
+        expectedCharacters = reversed as String
+        if (expectedCharacters.contains(">") || expectedCharacters.contains("]") || expectedCharacters.contains(")") || expectedCharacters.contains("}")) && lineComplete == false {
+            for char in expectedCharacters.indices {
+                linePoints *= 5
+                switch expectedCharacters[char] {
+                case ">":
+                    linePoints += 4
+                case "]":
+                    linePoints += 2
+                case ")":
+                    linePoints += 1
+                case "}":
+                    linePoints += 3
+                default:
+                    print("error")
+                }
+            }
+            points.append(linePoints)
+        } else {
+        }
+    }
+    points.sort()
+    print("All points: \(points)")
+    print("Middle points: \(points.middle!)")
 }
